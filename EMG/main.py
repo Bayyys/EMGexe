d1c6b4831b9cef0387d5db1b4428019c646abf21
bayyy <475417309@qq.com>
2023-04-13 09:28:11 +0800

    M 修改部分ui控件尺寸" bayyy-23.04.12

--- EMG/main.py
+++ EMG/main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QComboBox, QVBoxLayout, QWidget, QWidgetAction
-from PyQt5.QtCore import Qt, QDateTime, pyqtSignal, QThread, QMutex, QTimer
+from PyQt5.QtCore import Qt, QDateTime, pyqtSignal, QTimer
 from PyQt5 import uic
 # 串口操作
 import utils.serialUtil as serUtil
